680979bdd3c7bbaf27f439e6948aedb3126936fd
Rasmus Kaj <kaj@kth.se>
2010-10-04 21:11:42 +0200

    Short versions of the posts in the front page.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8; -*-
 from autoslug.fields import AutoSlugField
 from django.db import models
 
@@ -17,3 +18,23 @@ class Post(models.Model):
         return '/%d/%02d/%s' % (self.posted_time.year,
                                 self.posted_time.month,
                                 self.slug)
+    def linkedshort(self):
+        if len(self.content) < 600:
+            return self.content
+        
+        paras = self.content.split('\r\n\r\n')
+        lens = [len(p) for p in paras]
+
+        def result(paras):
+            print self.title, '=>', lens, '=>',  [len(p) for p in paras]
+            return u'%s ...\n\n"Läs mer":%s' % ('\n\n'.join(paras),
+                                               self.get_absolute_url())
+        
+        
+        for n in range(1,len(paras)):
+            if n > 1 and sum(lens[:n]) > 600:
+                return result(paras[:n-1])
+            if sum(lens[:n]) > 300:
+                return result(paras[:n])
+        
+        return self.content
