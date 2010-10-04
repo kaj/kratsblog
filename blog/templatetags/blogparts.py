d8f4398c6ee3ec836dd19c7b472302422ede575a
Rasmus Kaj <kaj@kth.se>
2010-10-04 07:19:46 +0200

    Added a titles by month list in the sidebar.

---
+++ blog/templatetags/blogparts.py
@@ -0,0 +1,11 @@
+from django import template
+
+from blog.models import Post
+
+register = template.Library()
+
+@register.inclusion_tag('blog/titles_by_month.html')
+def titles_by_month():
+    return {
+        'posts' : Post.objects.all().values('title', 'posted_time')
+        }
