3fa194f9df1e88f9731384b9f99494500629447b
Rasmus Kaj <kaj@kth.se>
2010-10-08 08:06:17 +0200

    Minor cleanup.

--- blog/models.py
+++ blog/models.py
@@ -18,26 +18,23 @@ class Post(models.Model):
         return u'%s (%s)' % (self.title, self.posted_time)
 
     def get_absolute_url(self):
-        return '/%d/%02d/%s' % (self.posted_time.year,
-                                self.posted_time.month,
+        return '/%d/%02d/%s' % (self.posted_time.year, self.posted_time.month,
                                 self.slug)
-    def linkedshort(self):
-        if len(self.content) < 600:
+    def linkedshort(self, maxlen=600):
+        if len(self.content) < maxlen:
             return self.content
         
         paras = self.content.split('\r\n\r\n')
         lens = [len(p) for p in paras]
 
         def result(paras):
-            print self.title, '=>', lens, '=>',  [len(p) for p in paras]
             return u'%s ...\n\n"Läs mer":%s' % ('\n\n'.join(paras),
                                                self.get_absolute_url())
         
-        
         for n in range(1,len(paras)):
-            if n > 1 and sum(lens[:n]) > 600:
+            if n > 1 and sum(lens[:n]) > maxlen:
                 return result(paras[:n-1])
-            if sum(lens[:n]) > 300:
+            if sum(lens[:n]) > maxlen/2:
                 return result(paras[:n])
         
         return self.content
