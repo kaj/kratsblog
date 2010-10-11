b3b825fc1b4c702403053b134dfc351c829fe04d
Rasmus Kaj <kaj@kth.se>
2010-10-11 10:56:35 +0200

    Add a heading to historic indexes.

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,23 @@
+# -*- encoding: utf-8 -*-
 from django.views.generic.simple import direct_to_template
 from django.shortcuts import get_object_or_404, get_list_or_404, redirect
 from blog.models import Post
+from django.template import defaultfilters
+from datetime import datetime
 
 def index(request, year=None, month=None):
     if year:
         all_posts = Post.objects.order_by('posted_time')
         if month:
+            head = u'Tygbittar från %s' % (
+               defaultfilters.date(datetime(int(year), int(month), 1), arg='F Y'))
             posts = get_list_or_404(all_posts, posted_time__year=year,
                                     posted_time__month=month)
         else:
+            head = u'Tygbittar från %s' % year
             posts = get_list_or_404(all_posts, posted_time__year=year)
     else:
+        head = None
         posts = Post.objects.all()[:5]
     # TODO Fix this with an annotation?
     for post in posts:
@@ -20,6 +27,7 @@ def index(request, year=None, month=None):
             post.ximg = None
     
     return direct_to_template(request, 'blog/index.html', {
+            'head': head,
             'posts': posts,
             'years': [x.year for x
                       in Post.objects.dates('posted_time', 'year')],
