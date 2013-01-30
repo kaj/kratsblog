6b10ccbb32aa62cae9bc31fde1f5d05a945b5824
Rasmus Kaj <kaj@kth.se>
2013-01-30 20:59:08 +0100

    A STATIC_ROOT default setting.

--- settings/defaults.py
+++ settings/defaults.py
@@ -95,6 +95,7 @@ TEMPLATE_DIRS = (
 )
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
