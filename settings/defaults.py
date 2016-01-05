2d8ed3795b6f4733deb74f626acca394aedae385
Rasmus Kaj <kaj@kth.se>
2016-01-05 23:09:12 +0100

    Update to django 1.9.
    * Update TEMPLATES setting to the dictionary format.
    
    * Update urlpatterns to a list of url()s.
    
    * (python-memcached seems broken above 1.54).

--- settings/defaults.py
+++ settings/defaults.py
@@ -27,6 +27,25 @@ CACHES = {
         }
     }
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages'
+            ],
+        },
+    },
+]
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -70,13 +89,6 @@ MEDIA_URL = '/media/'
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '=_rw+(azo19&5=7$3a5-ez$g4a&pfim75c4%f@s7rg^@f4!7c6'
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
-
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -86,13 +98,6 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'urls'
-
-TEMPLATE_DIRS = ( 
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_ROOT, 'templates'),
-)
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
--- urls.py
+++ urls.py
@@ -1,23 +1,17 @@
 from django.conf import settings
 from django.conf.urls import include, patterns, url
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
 from django.views.generic.base import TemplateView
+from django.views.static import serve
 admin.autodiscover()
 
 from blog import views as blog
 from blog import feeds
 from blog.sitemap import sitemaps
 
-urlpatterns = patterns(
-    '',
-    # Example:
-    # (r'^kratsblog/', include('kratsblog.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
 
     url(r'^$', blog.index),
     url(r'^(?P<year>[0-9]{4})/$', blog.index),
@@ -27,19 +21,17 @@ urlpatterns = patterns(
     url(r'^node/(?P<id>[0-9]+)', blog.redirect_from_id),
     url(r'^atom\.xml', feeds.AtomFeed()),
     url(r'^rss.xml', feeds.RssFeed()),
-    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap',
-        {'sitemaps': sitemaps}),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}),
     url(r'^robots\.txt$', TemplateView.as_view( 
             template_name='robots.txt', content_type='text/plain')),
     url(r'^PIE\.htc$', TemplateView.as_view(
         template_name='PIE.htc', content_type='text/x-component')),
-)
+]
 
 if settings.DEBUG and settings.MEDIA_URL[0] == '/':
-    urlpatterns += patterns(
-        '',
+    urlpatterns += [
         url('500', TemplateView.as_view(template_name='500.html')),
         url('404', TemplateView.as_view(template_name='404.html')),
-        (r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:], 
-         'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-    )
+        url(r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:],
+            serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
