84c00e77c70e87cddbf877a5766393abdb76918c
Rasmus Kaj <kaj@kth.se>
2017-12-18 00:03:27 +0100

    Add semi-usefull tests.

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 """
 This file demonstrates two different styles of tests (one doctest and one
 unittest). These will both pass when you run "manage.py test".
@@ -6,16 +7,23 @@ Replace these with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-from django.test.client import Client
+from blog.models import Post
+from datetime import datetime
+from lxml import html
 
 class SimpleTest(TestCase):
     
     def setUp(self):
-        self.c = Client()
+        Post.objects.create(
+            title='Hello World',
+            content='Hello there, this is a test article\n\n' +
+            'Containing two paragraphs of text.',
+            posted_time=datetime(2017, 12, 17, 23, 45, 0)
+        )
     
     def get(self, url, expected_status_code=200, expected_location='',
             expected_contenttype='text/html; charset=utf-8'):
-        response = self.c.get(url)
+        response = self.client.get(url)
         self.assertEqual((expected_status_code, expected_location, 
                           expected_contenttype),
                          (response.status_code,
@@ -23,26 +31,29 @@ class SimpleTest(TestCase):
                               .replace('http://testserver', ''),
                           response['Content-Type']))
 
-        # TODO Parse the document tree and do some useful stuff
-        #parser = html5lib.HTMLParser(tree=treebuilders.getTreeBuilder("lxml"),
-        #                             strict=True, namespaceHTMLElements=False)
-        
-        try:
-            return response.content
-            #if response.content:
-            #    return parser.parse(response.content)
-            #else:
-            #    return None
-
-        except Exception:
-            # NOTE Even though parser.errors is an array, it contains only first error.
-            self.fail("Response is NOT valid HTML5: %s" %
-                      ["%s, element: %s, pos: %s" % (error, element.get('name'), pos)
-                       for pos, error, element in parser.errors])
+        if response.content:
+            return html.document_fromstring(response.content)
+        else:
+            return None
         
     def test_get_frontpage(self):
         doc = self.get('/')
+        self.assertEquals(['Tygbittar.krats.se'], find_text(doc, 'h1'))
+        self.assertEquals(['Hello World'], find_text(doc, '#main h2'))
+        link, = doc.cssselect('#main h2 a')
+        self.assertEquals('/2017/12/hello-world', link.get('href'))
+
+    def test_get_article(self):
+        doc = self.get('/2017/12/hello-world')
+        self.assertEquals(['Hello World'], find_text(doc, '#main h2'))
+        self.assertEquals(['17 december 2017 23:45'], find_text(doc, '#main p.dateline'))
+        self.assertEquals(['Hello there, this is a test article',
+                           'Containing two paragraphs of text.'],
+                          find_text(doc, '#main .postContent p'))
 
     def test_get_feed(self):
         feed = self.get('/atom.xml',
                         expected_contenttype='application/atom+xml; charset=utf-8')
+
+def find_text(doc, selector):
+    return [e.text_content() for e in doc.cssselect(selector)]
